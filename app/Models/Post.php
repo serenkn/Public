f233e4dfcf1494705e7ea359191cdc5f1ab7b793
serenkn <seren-1219@yahoo.ne.jp>
2025-01-14 12:28:07 +0000

    test7-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,13 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    public function getByLimit(int $limit_count = 10)
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function getByLimit(int $limit_count = 3)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
 
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 3)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
